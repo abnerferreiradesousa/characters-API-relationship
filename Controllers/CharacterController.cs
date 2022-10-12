6c79674ffaf51fcc4f00a770f191d592d854c6b4
Abner <abnerferreira84@gmail.com>
2022-10-12 15:40:37 -0300

    feat: rota /Character do verbo GET que retorna os personagens pertencentes à um Id de um User.

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using EFCoreRelationshipsTutorial.Protocols;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFCoreRelationshipsTutorial.Controllers
 {
@@ -15,6 +16,15 @@ namespace EFCoreRelationshipsTutorial.Controllers
         public CharacterController(DataContext context)
         {
             _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Character>>> Get(int userId)
+        {
+            var characters = await _context.Characters
+                .Where<Character>(c => c.UserId == userId)
+                .ToListAsync();
+                
+            return Ok(characters);
         }
     }
 }
